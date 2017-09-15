34ebb2e752326461a1ec4fbfa6b43e647ba32df3
João Belo <jpasbelo@gmail.com>
2017-09-15 02:27:55 +0100

    upload file

--- include_files.php
+++ include_files.php
@@ -1,1 +1,14 @@
+<?php
 
+if ($handle = opendir('classes/')) {
+
+  // files to ignore
+  $blacklist = array('.', '..', 'somedir', 'index.php', '.DS_Store', 'unused');
+
+  while (false !== ($file = readdir($handle))) {
+        if (!in_array($file, $blacklist)) {
+            include ("classes/" . $file);
+        }
+    }
+    closedir($handle);
+}
